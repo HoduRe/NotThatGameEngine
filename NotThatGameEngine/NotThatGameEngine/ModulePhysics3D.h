5876ecc391d1108b81288438ece2252b9f3a4c5a
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2020-10-04 11:44:07 +0200

    MathGeoLib implemented

--- NotThatGameEngine/NotThatGameEngine/ModulePhysics3D.h
+++ NotThatGameEngine/NotThatGameEngine/ModulePhysics3D.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Module.h"
 #include "Globals.h"
-//#include "MathGeoLib/include/MathGeoLib.h"
+#include "MathGeoLib/include/MathGeoLib.h"
 
 class ModulePhysics3D : public Module
 {
