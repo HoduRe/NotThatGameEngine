c839d1d92641ddbe054f2eceac6a5f98fa2b41e9
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2020-10-27 23:12:41 +0100

    I saw the ahegao after like 2 hours of work, and I'm laughing at my own stupid meme. I love programming

--- NotThatGameEngine/NotThatGameEngine/Camera3D.cpp
+++ NotThatGameEngine/NotThatGameEngine/Camera3D.cpp
@@ -1,102 +1,93 @@
 #include "Application.h"
 
-Camera3D::Camera3D(Application* app, bool start_enabled) : Module(app, start_enabled)
-{
+Camera3D::Camera3D(Application* app, bool start_enabled) : Module(app, start_enabled) {
+
 	CalculateViewMatrix();
 
 	X = vec3(1.0f, 0.0f, 0.0f);
 	Y = vec3(0.0f, 1.0f, 0.0f);
 	Z = vec3(0.0f, 0.0f, 1.0f);
-
 	Position = vec3(0.0f, 0.0f, 5.0f);
 	Reference = vec3(0.0f, 0.0f, 0.0f);
 
 }
 
-Camera3D::~Camera3D()
-{}
 
-// -----------------------------------------------------------------
-bool Camera3D::Start()
-{
+Camera3D::~Camera3D() {}
+
+
+bool Camera3D::Start() {
+
 	bool ret = true;
-
 	return ret;
+
 }
 
-// -----------------------------------------------------------------
-bool Camera3D::CleanUp()
-{
-	return true;
-}
 
-// -----------------------------------------------------------------
+bool Camera3D::CleanUp() { return true; }
+
+
 update_status Camera3D::Update(float dt)
 {
 	vec3 newPos(0, 0, 0);
-	float speed = 3.0f * dt;
-	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-		speed = 8.0f * dt;
+	float speed = 5.0f * dt;
 
-	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
-	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;
+	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) { speed = 10.0f * dt; }
+	if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT) {
 
-	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
-	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
-
-
-	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
-	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += X * speed;
-
-	Position += newPos;
-	Reference += newPos;
-
-	// Mouse motion ----------------
-
-	if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
-	{
 		int dx = -App->input->GetMouseXMotion();
 		int dy = -App->input->GetMouseYMotion();
-
 		float Sensitivity = 0.25f;
 
+		if (App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT) newPos.y += speed;
+		if (App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT) newPos.y -= speed;
+		if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
+		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
+		if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
+		if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += X * speed;
+
+		Position += newPos;
+		Reference += newPos;
 		Position -= Reference;
 
-		if (dx != 0)
-		{
-			float DeltaX = (float)dx * Sensitivity;
+		if (dx != 0) {
 
+			float DeltaX = (float)dx * Sensitivity;
 			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
 			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
 			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+
 		}
 
-		if (dy != 0)
-		{
-			float DeltaY = (float)dy * Sensitivity;
+		if (dy != 0) {
 
+			float DeltaY = (float)dy * Sensitivity;
 			Y = rotate(Y, DeltaY, X);
 			Z = rotate(Z, DeltaY, X);
 
-			if (Y.y < 0.0f)
-			{
+			if (Y.y < 0.0f) {
+
 				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
 				Y = cross(Z, X);
+
 			}
 		}
 
 		Position = Reference + Z * length(Position);
 	}
 
-	// Recalculate matrix -------------
+	else if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT && App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT) {
+		// LookAt(vec3 (3, 4, 2));		This should do... stuff. Rotate around the center of the selected object
+	}
+
 	CalculateViewMatrix();
 
 	return update_status::UPDATE_CONTINUE;
 }
 
-// -----------------------------------------------------------------
-void Camera3D::Look(const vec3& Position, const vec3& Reference, bool RotateAroundReference)
-{
+
+void Camera3D::Look(const vec3& Position, const vec3& Reference, bool RotateAroundReference) {
+
 	this->Position = Position;
 	this->Reference = Reference;
 
@@ -104,46 +95,47 @@ void Camera3D::Look(const vec3& Position, const vec3& Reference, bool RotateArou
 	X = normalize(cross(vec3(0.0f, 1.0f, 0.0f), Z));
 	Y = cross(Z, X);
 
-	if (!RotateAroundReference)
-	{
+	if (!RotateAroundReference) {
+
 		this->Reference = this->Position;
 		this->Position += Z * 0.05f;
+
 	}
 
 	CalculateViewMatrix();
+
 }
 
-// -----------------------------------------------------------------
-void Camera3D::LookAt(const vec3& Spot)
-{
-	Reference = Spot;
 
+void Camera3D::LookAt(const vec3& Spot) {
+
+	Reference = Spot;
 	Z = normalize(Position - Reference);
 	X = normalize(cross(vec3(0.0f, 1.0f, 0.0f), Z));
 	Y = cross(Z, X);
-
 	CalculateViewMatrix();
+
 }
 
 
-// -----------------------------------------------------------------
-void Camera3D::Move(const vec3& Movement)
-{
+void Camera3D::Move(const vec3& Movement) {
+
 	Position += Movement;
 	Reference += Movement;
-
 	CalculateViewMatrix();
+
 }
 
-// -----------------------------------------------------------------
-float* Camera3D::GetViewMatrix()
-{
-	return &ViewMatrix;
-}
 
-// -----------------------------------------------------------------
-void Camera3D::CalculateViewMatrix()
-{
+float* Camera3D::GetViewMatrix() { return &ViewMatrix; }
+
+
+void Camera3D::CalculateViewMatrix() {
+
 	ViewMatrix = mat4x4(X.x, Y.x, Z.x, 0.0f, X.y, Y.y, Z.y, 0.0f, X.z, Y.z, Z.z, 0.0f, -dot(X, Position), -dot(Y, Position), -dot(Z, Position), 1.0f);
 	ViewMatrixInverse = inverse(ViewMatrix);
+
 }
+
+
+
